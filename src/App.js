1333d0da9ef3c9254aa12ce16ef7181fd243347c
Abu Bakar Khan <khani.abubakar1991@gmail.com>
2021-10-16 12:53:49 +0500

    just checking

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div>
-        <h1> Alhamdulilah! My First React App.</h1>
+        <h1> Alhamdulilah! If it works.</h1>
+        <h1> Alhamdulilah! If it not works.</h1>
     </div>
   );
 }
