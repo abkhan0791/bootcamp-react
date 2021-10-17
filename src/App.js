aacae381ed30c4f507392a2ac7ed026c354a0a60
Abu Bakar Khan <khani.abubakar1991@gmail.com>
2021-10-17 07:29:42 +0500

    center line

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
         <h1> Alhamdulilah! If it works.</h1>
         <h1> Alhamdulilah! If it not works.</h1>
     </div>
