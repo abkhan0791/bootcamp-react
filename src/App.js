74b7aca3beb1497ebc78526fed03cc23158df4a4
Abu Bakar Khan <khani.abubakar1991@gmail.com>
2021-10-18 19:01:46 +0500

    button learning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Namaz from './namaz.js';
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(5)
+  let [isColor, setColor] = useState(false)
   return (
-    <div className='App'>
-      <Namaz ftime = '5:20 AM' ztime = '1:00 PM' atime = '3:45 PM' mtime = '5:40 PM' itime = '7:45 PM'/>
+    <div className={`body ${isColor ? 'App' : ''}`}>
+      <Namaz counter = {count}/>
+      <button onClick = {()=> setCount(++count)}>
+        Update
+      </button>
+
+      <br/>
+      <br/>
+
+      <button onClick = {()=> setColor(!isColor)}>
+        Change Color
+      </button>
     </div>
   );
 }
--- src/namaz.js
+++ src/namaz.js
@@ -3,12 +3,7 @@ import React from 'react';
 function Namaz(props) {
     return(
         <div>
-            <h1> This is Salah TimeTable. Please remeber it. Thank you</h1>
-            <h2> Fajar {props.ftime}</h2>
-            <h2> Zuhar {props.ztime}</h2>
-            <h2> Asar {props.atime}</h2>
-            <h2> Maghrib {props.mtime}</h2>
-            <h2> Isha {props.itime}</h2>
+            <h1> Today Fajar Time is : {props.counter}</h1>
         </div>
     )
 }
